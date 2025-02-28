2e5381170a15589ea0e323bf7c388c89a878c128
Kopp <Kopp.Darin@epa.gov>
2025-02-28 05:04:27 -0800

    Added Comments

--- Extrapolation_Dissimilarity_Index.R
+++ Extrapolation_Dissimilarity_Index.R
@@ -1,6 +1,6 @@
 # Checking for extrapolation
 
-setwd("Raw_Data/Extrapolation")
+setwd("Extrapolation")
 
 library(readxl)
 library(parallel)
@@ -26,22 +26,26 @@ Importance <- PredImpFilter
 # function to scale and weight predictor variables
 Scale_Weight <- function(TrainData, Importance){
   
-  #scale predictor variables
+  #scale parameters predictor variables
   ScaleParms <- apply(TrainData[, names(Importance)], 2, 
                       function(x) c(center = mean(x), 
                                     scale = sd(x)))
-  
+  #scale predictor variables
   TrainDataScaled <- sapply(
     colnames(ScaleParms), function(x) 
       scale(TrainData[,x],
             center = ScaleParms["center",x],
             scale = ScaleParms["scale",x]))
   
+  # apply non-standardized imporance as weight
   TrainDataScaledWeighted <- t(apply(
     TrainDataScaled[,names(Importance)], 1,
     function(x) x*Importance))
   
-  meanDist <- mean(vegan::vegdist(TrainDataScaledWeighted, method = "euclidean"))
+  # calculate mean distance among all training data in 
+  # predictor space
+  # Rfast::Dist(df, method = "euclidean")
+  meanDist <- mean(dist(TrainDataScaledWeighted))
   
   return(list(ScaleParms = ScaleParms, 
               TrainDataScaledWeighted = TrainDataScaledWeighted, 
@@ -51,6 +55,7 @@ Scale_Weight <- function(TrainData, Importance){
 # function to calculate dissimilarity index
 Test4Extrapolation <- function(TrainDataWeightedList, NewDataPt, Importance){
   
+  # a reminder that the scale weight function should be run first
   if(!is.list(TrainDataWeightedList)){
     stop("TrainDataWeighted needs to be names list")
   }
@@ -59,36 +64,39 @@ Test4Extrapolation <- function(TrainDataWeightedList, NewDataPt, Importance){
   TrainDataScaledWeighted <- TrainDataWeightedList[["TrainDataScaledWeighted"]]
   MeanDist <- TrainDataWeightedList[["MeanDist"]]
     
+  #apply scale parameters to new data
   NewDataScaled <- sapply(
-    #colnames(ScaleParms),
     1:ncol(ScaleParms),
     function(x) 
       scale(NewDataPt[x],
             center = ScaleParms["center", x],
             scale = ScaleParms["scale", x]))
   
-  
+  # weight new data as importance 
   NewDataScaledWeighted <- NewDataScaled*Importance
-  t1 <- Sys.time()
+  
+  # calculate distance between new data point and all training data
   d2 <- sapply(1:nrow(TrainDataScaledWeighted), function(n){
     #n<-1
     df <- rbind(NewDataScaledWeighted, TrainDataScaledWeighted[n,])
     #d <- vegan::vegdist(df, method = "euclidean")
     #d <- stats::dist(df)
+    # Rfast runs is much faster than other options
     d <- max(Rfast::Dist(df, method = "euclidean"))
     return(d)
   })
-  t2 <- Sys.time()
-  t2-t1
   
+  # calculate the dissimilarity index 
   DI <- min(d2)/MeanDist
+  
   return(min(DI))
 }
 
 # scale and weight training data
 TrainList <- Scale_Weight(TrainData, Importance)
 
-
+# calculate dissimilarity index for new data
+# estimated time for 8million records ~4days
 cl <- makeCluster(detectCores())
 clusterExport(cl, c("TrainList", "Importance", "Test4Extrapolation"))
 t1 <- Sys.time()
@@ -101,77 +109,3 @@ t2 <- Sys.time()
 stopCluster(cl)
 t2-t1
 
-
-#######
-Test4Extrapolation(TrainDataWeightedList = TrainList, 
-                   NewDataPt = x[1, names(Importance)], 
-                   Importance = Importance)
-install.packages("fastDist")
-nrow(TrainList[["TrainDataScaledWeighted"]][,-1])
-
-sapply(1:12, function(x){
-  Test4Extrapolation(TrainDataWeightedList = TrainList, 
-                     NewDataPt = NewData[x,names(Importance)], 
-                     Importance = Importance)
-  
-})
-
-apply(NewData[1:13, names(Importance)],1, function(x){
-  Test4Extrapolation(TrainDataWeightedList = TrainList, 
-                     NewDataPt = x, 
-                     Importance = Importance)
-})
-
-NewDataPt<-NewData[1, names(Importance)]
-for (i in 1:13){
-  di <- Test4Extrapolation(TrainDataWeightedList = TrainList, 
-                           NewDataPt = NewData[i,], 
-                           Importance)  
-}
-
-
-#scale predictor variables
-########################
-ScaleParms <- apply(TrainData[, -1], 2, 
-                    function(x) c(center = mean(x), scale = sd(x)))
-
-TrainDataScaled <- sapply(
-  colnames(ScaleParms), function(x) 
-    scale(TrainData[,x],
-          center = ScaleParms["center",x],
-          scale = ScaleParms["scale",x]))
-
-NewDataScaled <- sapply(
-  colnames(ScaleParms), function(x) 
-    scale(NewData[,x],
-          center = ScaleParms["center",x],
-          scale = ScaleParms["scale",x]))
-
-PredImpFilter <- PredImp[PredImp$class=="fungicides",]
-#predictors in importance are not included in the training data?
-# PredImpFilter$pred[!PredImpFilter$pred%in%colnames(TrainDataScaled)]
-preds <- intersect(PredImpFilter$pred,colnames(TrainDataScaled))
-PredImpFilter <- PredImpFilter[PredImpFilter$pred%in%preds,]
-PredImpFilter <- setNames(PredImpFilter$V1,PredImpFilter$pred)
-
-TrainDataScaledWeighted <- t(apply(
-  TrainDataScaled[,names(PredImpFilter)], 1,
-  function(x) x*PredImpFilter))
-
-NewDataScaledWeighted <- t(apply(
-  NewDataScaled[,names(PredImpFilter)], 1,
-  function(x) x*PredImpFilter))
-
-
-for (i in 1:10){
-  NewDataPt <- NewDataScaledWeighted[i,]
-  d2 <- sapply(1:nrow(TrainDataScaledWeighted), function(n){
-    df <- rbind(NewDataPt,
-                TrainDataScaledWeighted[n,])
-    d <- vegan::vegdist(df, method = "euclidean")
-    return(d)
-  })
-  
-  print(min(d2))
-}
-###########################################################
